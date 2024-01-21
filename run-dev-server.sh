169bd6ed8f96b83a496f2c193fc81d385e9d6489
Rimas Kudelis <rimas@kudelis.lt>
2024-01-21 20:23:41 +0200

    Leidžiam lokalų serverį ant 80 porto

--- run-dev-server.sh
+++ run-dev-server.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-# Runs Hugo server, accessible at http://localhost:1313
-docker run -p 1313:1313 -u $(id -u):$(id -g) -v ${PWD}:/src hugomods/hugo:exts hugo server --bind 0.0.0.0
+# Runs Hugo server, accessible at http://localhost (port 80)
+docker run -p 127.0.0.1:80:1313 -u $(id -u):$(id -g) -v ${PWD}:/src hugomods/hugo:exts hugo server --bind 0.0.0.0
