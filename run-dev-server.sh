d9fd14893de78e4fc8aa5d4db2a74d39f76b3ca2
Rimas Kudelis <rimas@kudelis.lt>
2024-01-26 23:19:24 +0200

    Nurodom portą per ENV ir naudojam tą patį tiek Hugo, tiek Dokeriui
    Kitaip automatinis refresh'as po pakeitimų neveikia.

--- run-dev-server.sh
+++ run-dev-server.sh
@@ -1,3 +1,8 @@
-#/bin/sh
-# Runs Hugo server, accessible at http://localhost (port 80)
-docker run -p 127.0.0.1:80:1313 -u $(id -u):$(id -g) -v ${PWD}:/src hugomods/hugo:exts hugo server --bind 0.0.0.0
+#!/bin/sh
+
+if [ -z "$PORT" ]; then
+    PORT=80
+fi
+
+# Runs Hugo server, accessible at http://localhost:$PORT (default to port 80)
+docker run -p 127.0.0.1:${PORT}:${PORT} -u $(id -u):$(id -g) -v ${PWD}:/src hugomods/hugo:exts hugo server --bind 0.0.0.0 --port ${PORT}
